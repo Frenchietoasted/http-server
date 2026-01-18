f96141fca3f786078e5c16c69cac051964ba89d3
Steve Dmello <steveaarond@gmail.com>
2026-01-18 19:14:44 +0530

    Did something

--- server.c
+++ server.c
@@ -1,4 +1,4 @@
-//run using 'gcc server.c -o server -lws2_32 -lmswsock' then ./server
+// run using 'gcc server.c -o server -lws2_32 -lmswsock' then ./server
 #include <stdio.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -6,12 +6,16 @@
 
 void serveClient(SOCKET);
 HANDLE getFile(SOCKET, char *);
-int sendFile(SOCKET, HANDLE,char *);
+int sendFile(SOCKET, HANDLE, char *);
+const char *getMIME(char *);
 
+int handleRequest(SOCKET, char[]);
+int handleFile(SOCKET, char *);
+void handleRoute(SOCKET, char *);
 int main()
 {
     WSADATA wsa;
-    int count=1;
+    int count = 1;
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
     {
         WSACleanup();
@@ -35,7 +39,7 @@ int main()
     while (1)
     {
         serveClient(s);
-        printf("\n%d Requests Done\n",count++);
+        printf("\n%d request Done\n", count++);
     }
 
     int err = WSAGetLastError(); // Check for errors if any
@@ -57,49 +61,63 @@ void serveClient(SOCKET s)
         printf("accept failed: %d\n", WSAGetLastError());
         return;
     }
-    char buffer[1028] = {0};
+    char buffer[1024] = {0};
+    int recvBytes;
+    recvBytes = recv(client, buffer, 1024, 0);
+    printf("\nBytes recieved:%d\n",recvBytes);
+    if (recvBytes <= 0)return;
+    handleRequest(client, buffer);
 
-    int recvBytes = recv(client, buffer, 1028, 0);
-    printf("%s",buffer);
-    char *file = buffer+5;
-    *strchr(file, ' ') = 0; // removes the blank space at the end of the filename in the https request
-    printf("\nBytes: %d \nServer recieved: %s", recvBytes, file);
-    if(!strcmp(file,"favicon.ico")) {
-        shutdown(client, SD_SEND);
-        closesocket(client);
-        return;
-    }
-    if(strlen(file)==0){
-        file="file.html";
-        printf("\nrerouted %s",file);
-    }
-    HANDLE hFile = getFile(client, file);
 
-    if (hFile == INVALID_HANDLE_VALUE)
-    {
-        DWORD err = GetLastError();
-        printf("\nerror in file Handle");
-        send(client,
-             "HTTP/1.1 404 Not Found\r\n"
-             "Content-Type: text/plain\r\n"
-             "Connection: close\r\n"
-             "\r\n"
-             "404 Not Found",
-             78,
-             0);
-        closesocket(client);
-        return;
-    }
-
-    if (!sendFile(client, hFile,file))
-    {
-        printf("\nTransmit failed: %d",WSAGetLastError());
-        return;
-    }
-    
-    CloseHandle(hFile);
-    shutdown(client, SD_SEND);
+    shutdown(client, SD_BOTH);
     closesocket(client);
+}
+
+int handleRequest(SOCKET client, char buffer[])
+{
+    printf("%20s", buffer);
+    char *connection = strstr(buffer, "Connection:");
+    char *route = strchr(buffer, '/') + 1;
+    int returnStatus;
+    if (route[0] == ' ')
+    {
+        route = "file.html";
+        printf("\nrerouted %s", route);
+    }
+    else
+    {
+        *strchr(route, ' ') = 0; // removes the blank space at the end of the filename in the https route
+        printf("\nServer recieved: %s", route);
+    }
+
+    // TODO: route implementation
+    //  if(strchr(route,'.') == NULL)
+    //  {
+    //      printf("We want a route");
+    //  }
+    //  else{
+    printf("\nWe want a file");
+    returnStatus = handleFile(client, route);
+    // }
+    if (returnStatus != 0)
+    {
+        printf("Error :%d", returnStatus);
+        return 2;
+    }
+    if (connection == NULL)
+        return 0;
+    else
+    {
+        connection += 12;
+        *strchr(connection, '\r') = 0;
+        printf("\nConnection:%s", connection);
+    }
+    if (!strcmp(connection, "keep-alive"))
+    {
+        printf("\nkeeping alive");
+        return 0;
+    }
+    return 1;
 }
 
 HANDLE getFile(SOCKET client, char *file)
@@ -122,21 +140,29 @@ int sendFile(SOCKET client, HANDLE hFile, char *file)
     GetFileSizeEx(hFile, &fileSize);
 
     printf("\nExtracting extention: ");
-    char *extention=strchr(file, '.')+1; //point to the character after the dot(.)
-    printf("\n%s",extention);
-    
+    char *extention = strchr(file, '.'); // get the extention
+    if (!extention)
+        return 1;
+    else
+        extention++; // point to the character after the dot(.)
+    printf("\n%s", extention);
+    if (!strcmp(extention, "ico"))
+        return 1;
     char header[256];
-    
+    char *connection=strcmp(extention,"css")==0?"Closed":"keep-alive";
     _snprintf_s(
         header,
         sizeof(header),
         _TRUNCATE,
         "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/%s\r\n"
+        "Content-Type: %s\r\n"
         "Content-Length: %lld\r\n"
-        "Connection: close\r\n"
-        "\r\n",extention,
-        fileSize.QuadPart);
+        "Connection: %s\r\n"
+        "\r\n",
+        getMIME(extention),
+        fileSize.QuadPart,
+        connection
+    );
 
     BOOL noDelay = TRUE;
     setsockopt(client, IPPROTO_TCP, TCP_NODELAY, (const char *)&noDelay, sizeof(noDelay));
@@ -144,4 +170,54 @@ int sendFile(SOCKET client, HANDLE hFile, char *file)
     send(client, header, strlen(header), 0);
     int success = TransmitFile(client, hFile, 0, 0, NULL, NULL, 0);
     return success;
+}
+
+const char *getMIME(char *ext)
+{
+    if (!strcmp(ext, "html"))
+        return "text/html";
+    if (!strcmp(ext, "css"))
+        return "text/css";
+    if (!strcmp(ext, "js"))
+        return "application/javascript";
+    if (!strcmp(ext, "png"))
+        return "image/png";
+    if (!strcmp(ext, "jpg"))
+        return "image/jpeg";
+    if (!strcmp(ext, "ico"))
+        return "image/x-icon";
+    return "application/octet-stream";
+}
+
+int handleFile(SOCKET client, char *route)
+{
+    HANDLE hFile = getFile(client, route);
+
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        DWORD err = GetLastError();
+        printf("\nerror in file Handle");
+        send(client,
+             "HTTP/1.1 404 Not Found\r\n"
+             "Content-Type: text/plain\r\n"
+             "Connection: close\r\n"
+             "\r\n"
+             "404 Not Found",
+             78,
+             0);
+
+        return 404;
+    }
+
+    if (!sendFile(client, hFile, route))
+    {
+        int err = WSAGetLastError();
+        if (err != WSAECONNRESET)
+            printf("\nTransmit failed: %d", err);
+        return err;
+    }
+
+    CloseHandle(hFile);
+    printf("\nClosed file handle");
+    return 0;
 }
